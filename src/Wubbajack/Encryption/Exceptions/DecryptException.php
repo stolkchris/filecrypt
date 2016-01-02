13ee7d95ae9e8ad4253c820465a02111a0ba45d8
Christopher Stolk <stolkchris@gmail.com>
2016-01-02 22:01:31 +0000

    Added source code of FileEncrypter classes

---
+++ src/Wubbajack/Encryption/Exceptions/DecryptException.php
@@ -0,0 +1,14 @@
+<?php namespace Wubbajack\Encryption\Exceptions;
+
+/**
+ * Class DecryptException
+ *
+ * @package Wubbajack\Encryption\Exceptions
+ * @author  Chris Stolk <stolkchris@gmail.com>
+ * @license MIT <https://opensource.org/licenses/MIT>
+ * @since   0.0.1
+ */
+class DecryptException extends \Exception
+{
+
+}
---
+++ src/Wubbajack/Encryption/Exceptions/EncryptException.php
@@ -0,0 +1,14 @@
+<?php namespace Wubbajack\Encryption\Exceptions;
+
+/**
+ * Class EncryptException
+ *
+ * @package Wubbajack\Encryption\Exceptions
+ * @author  Chris Stolk <stolkchris@gmail.com>
+ * @license MIT <https://opensource.org/licenses/MIT>
+ * @since   0.0.1
+ */
+class EncryptException extends \Exception
+{
+
+}
